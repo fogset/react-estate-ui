9db7707d8c0a001b4e853f4b3eea422bcde15efe
Tianhao Zhang <zhangth1990@hotmail.com>
2024-08-16 16:38:03 -0600

    added filter section

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,7 +1,14 @@
 import "./singlePage.scss";
+import { listData } from "../../lib/dummydata";
 
 function SinglePage() {
-    return <div className="singlePage">SinglePage</div>;
+    const data = listData;
+    return (
+        <div className="listPage">
+            <div className="listContainer">List</div>
+            <div className="mapContainer">Map</div>
+        </div>
+    );
 }
 
 export default SinglePage;
