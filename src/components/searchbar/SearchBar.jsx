673f940c9f7dd091d2745b66ce245c03255d73a0
Tianhao Zhang <zhangth1990@hotmail.com>
2024-08-15 18:01:07 -0600

    add searchbar

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
 function SearchBar() {
-    return <div className="searchBar">SearchBar</div>;
+    const [query, setQuery] = useState({
+        type: "buy",
+        location: "",
+        minPrice: 0,
+        maxPrice: 0,
+    });
+    function switchType(val) {
+        setQuery((prev) => ({ ...SearchBar.prev, type: val }));
+    }
+    return (
+        <div className="searchBar">
+            <div className="type">
+                {types.map((type) => (
+                    <button
+                        key={type}
+                        onClick={() => switchType(type)}
+                        className={query.type === type ? "active" : ""}
+                    >
+                        {type}
+                    </button>
+                ))}
+            </div>
+            <form>
+                <input type="text" name="location" placeholder="City Location" />
+                <input
+                    type="number"
+                    name="minPrice"
+                    placeholder="Min Price"
+                    min={0}
+                    max={10000000}
+                />
+                <input
+                    type="number"
+                    name="maxPrice"
+                    min={0}
+                    max={10000000}
+                    placeholder="Max Price"
+                />
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default SearchBar;
