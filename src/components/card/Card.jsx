9235c0f76a35a5f9656e9f608088eaac79bda9ff
Tianhao Zhang <zhangth1990@hotmail.com>
2024-08-16 22:45:52 -0600

    add card section

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
 import "./card.scss";
 
-function Card() {
-    return <div className="card">Card</div>;
+function Card({ item }) {
+    return (
+        <Link to={`/${item.id}`} className="card">
+            <div className="imageContainer">
+                <img src={item.img} alt="" />
+            </div>
+            <div className="textContainer">
+                <h2 className="title">{item.title}</h2>
+                <p className="address">
+                    <img src="/pin.png" alt="" />
+                    <span>{item.address}</span>
+                </p>
+                <p className="price">$ {item.price}</p>
+                <div className="bottom">
+                    <div className="features">
+                        <div className="feature">
+                            <img src="/bed.png" alt="" />
+                            <span>{item.bedroom} bedroom</span>
+                        </div>
+                        <div className="feature">
+                            <img src="/bath.png" alt="" />
+                            <span>{item.bedroom} bedroom</span>
+                        </div>
+                    </div>
+                    <div className="icons">
+                        <div className="icon">
+                            <img src="/save.png" alt="" />
+                        </div>
+                        <div className="icon">
+                            <img src="/chat.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
 }
 
 export default Card;
