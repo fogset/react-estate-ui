50ec3012d53f182ad92c62f68d0b80cfbfc798ec
Tianhao Zhang <zhangth1990@hotmail.com>
2024-08-20 22:37:22 -0600

    add chat

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./chat.scss";
 
 function Chat() {
+    const [chat, setChat] = useState(true);
     return (
         <div className="chat">
             <div className="messages">
@@ -46,7 +48,52 @@ function Chat() {
                     <p>lsldfkjasl;dfkjasldkfjsd....</p>
                 </div>
             </div>
-            <div className="chatBox"></div>
+            {chat && (
+                <div className="chatBox">
+                    <div className="top">
+                        <div className="user">
+                            <img
+                                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                                alt=""
+                            />
+                            John Doe
+                        </div>
+                        <span className="close" onClick={() => setChat(null)}>
+                            X
+                        </span>
+                    </div>
+                    <div className="center">
+                        <div className="chatMessage own">
+                            <p>Mainly cloudy. 60 noon. High 20. UV ow</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Mainly cloudy. 60 noon. High 20. UV ow</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Mainly cloudy. 60 noon. High 20. UV ow</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Mainly cloudy. 60 noon. High 20. UV ow</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Mainly cloudy. 60 noon. High 20. UV ow</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Mainly cloudy. 60 noon. High 20. UV ow</p>
+                            <span>1 hour ago</span>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <textarea></textarea>
+                        <button>Send</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
