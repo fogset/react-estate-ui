449fc5b71deecd93f5350d0b9515c6195421b415
Tianhao Zhang <zhangth1990@hotmail.com>
2024-09-02 15:16:38 -0600

    added page login and register

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profile/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
 
 function App() {
     const router = createBrowserRouter([
@@ -26,6 +28,14 @@ function App() {
                 {
                     path: "/profile",
                     element: <ProfilePage />,
+                },
+                {
+                    path: "/login",
+                    element: <Login />,
+                },
+                {
+                    path: "/register",
+                    element: <Register />,
                 },
             ],
         },
